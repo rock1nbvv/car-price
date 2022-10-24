9a4c2ecde997178054119b858aab3c3dce3ee0bb
Vlad Baklaiev <vlad.baklaiev@strikersoft.com>
2022-10-24 17:17:29 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from flask import request
+from flask import redirect
+from flask import url_for
+
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+
+load_dotenv('./.flaskenv')
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+import models
+from forms import TaskForm
+
+
+@app.route('/')
+def index():
+    tasks = models.Task.query.all()
+
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return jsonify(tasks)
+
+    return render_template('index.html')
+
+
+@app.route('/create', methods=['POST'])
+def create_task():
+    user_input = request.get_json()
+
+    form = TaskForm(data=user_input)
+
+    if form.validate():
+        task = models.Task(title=form.title.data)
+
+        db.session.add(task)
+        db.session.commit()
+
+        return jsonify(task)
+
+    return redirect(url_for(index))
+
+
+@app.route('/delete', methods=['POST'])
+def delete_task():
+    task_id = request.get_json().get('id')
+    task = models.Task.query.filter_by(id=task_id).first()
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return jsonify({'result': 'Ok'}), 200
+
+
+@app.route('/complete', methods=['POST'])
+def complete_task():
+    task_id = request.get_json().get('id')
+    task = models.Task.query.filter_by(id=task_id).first()
+
+    task.completed = True
+
+    db.session.add(task)
+    db.session.commit()
+
+    return jsonify({'result': 'Ok'}), 200
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/main.js
@@ -0,0 +1,89 @@
+const {createApp} = Vue
+
+const TaskApp = {
+    data() {
+        return {
+            task: {
+                'title': ''
+            },
+            tasks: [],
+            selected: '',
+            age: '',
+            mileage: '',
+            repairments: '',
+            options: [
+                {text: 'One', value: 'A'},
+                {text: 'Two', value: 'B'},
+                {text: 'Three', value: 'C'},
+                {text: 'Four', value: 'D'}
+            ]
+        }
+
+    },
+    async created() {
+        await this.getTasks()
+    },
+    methods: {
+        async sendRequest(url, method, data) {
+            const myHeaders = new Headers({
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest'
+            })
+
+            const response = await fetch(url, {
+                method: method,
+                headers: myHeaders,
+                body: data
+            })
+
+            return response
+        },
+        async getTasks() {
+            const response = await this.sendRequest(window.location, 'get')
+            this.tasks = await response.json()
+
+        },
+        async createTask() {
+            await this.getTasks()
+
+            await this.sendRequest(window.location + 'create', 'post', JSON.stringify(this.task))
+
+            await this.getTasks()
+
+            this.task.title = ''
+        },
+        async deleteTask(task) {
+            await this.sendRequest(window.location + 'delete', 'post', JSON.stringify(task))
+            await this.getTasks()
+        },
+        async completeTask(task) {
+            await this.sendRequest(window.location + 'complete', 'post', JSON.stringify(task))
+            await this.getTasks()
+        },
+        validateNumber: (event) => {
+            let keyCode = event.keyCode;
+            if (keyCode < 48 || keyCode > 57) {
+                event.preventDefault();
+            }
+        },
+        printCar(Car) {
+            console.log('Car age is ' + Car.age)
+            console.log('Car mileage is ' + Car.mileage)
+            console.log('Car repairments number is ' + Car.repairments)
+
+        },
+        warn(age, mileage, repairments) {
+            if (this.age === '' || this.age === null || this.age.value === 0
+                || this.mileage === '' || this.mileage === null || this.mileage.value === 0
+                || this.repairments === '' || this.repairments === null || this.repairments.value === 0) {
+                alert('Please fill all the fields');
+            } else {
+                alert(age + ' ' + mileage + ' ' + repairments)
+                this.printCar({age: age, mileage: mileage, repairments: repairments})
+            }
+        }
+    },
+    delimiters: ['{', '}']
+}
+
+createApp(TaskApp).mount('#app')
